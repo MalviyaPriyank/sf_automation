0cdf1061c678361682a3f5645c99d3887378ab83
Sharmin Pathan <sharmin@Sharmins-MacBook-Air.local>
2024-12-22 13:48:56 -0800

    streamlit addition test

--- .ipynb_checkpoints/snowchain-checkpoint.py
+++ .ipynb_checkpoints/snowchain-checkpoint.py
@@ -1,3 +1,14 @@
 import streamlit as st
+from src.tools import math_tools
+
 
 st.title('Snowchain - Test Env')
+
+with st.chat_message("assistant"):
+    st.write("Please type two numbers separated by space, for addition")
+
+if prompt := st.chat_input("How can I assist you today?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        num1, num2 = prompt.split(' ')
+        st.markdown(math_tools.addition(num1, num2))
---
+++ src/math_tools.py
@@ -0,0 +1,6 @@
+
+def addition(num1: int, 
+             num2: int) -> str:
+    """Calculates addition of two numbers"""
+    return f"The addition of {num1} and {num2} is {int(num1)+int(num2)}"
+    
