6b6fd9510b9f2c06276031f40128777d96a86a0f
Priyank Malviya <priyankmalviya0@gmail.com>
2025-01-19 10:20:39 -0500

    added config setup for snowchain

--- src/setup/initial.py
+++ src/setup/initial.py
@@ -4,22 +4,40 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__),'../obj'))
 sys.path.append(os.path.join(os.path.dirname(__file__),'../vars'))
 
+from global_vars import Role as gv_rl
+from global_vars import Warehouse as gv_wh
+from global_vars import Config as cfg
 from role import Role
 from warehouse import Warehouse
-from global_vars import Role as RL 
-from global_vars import Warehouse as WH 
+from database import Database
+from schema import Schema
+from internalstage import InternalStage
+
  
 def create_default_role(session):
-    for role,description in RL._default_role.items():
+    for role,description in gv_rl._default_role.items():
         Role.create_object(session,{"NAME":role,"COMMENT":description})
 
 def create_default_warehouse(session):
-    for warehouse_name,warehouse_size in WH.__default_warehouse.items():
+    for warehouse_name,warehouse_size in gv_wh.__default_warehouse.items():
         Warehouse.create_object(session,{"NAME":warehouse_name,"TYPE":"STANDARD","SIZE": warehouse_size})
+
+def create_config_database(session):
+    Database.create_object(session,{"NAME":cfg._config_database})
+
+def create_config_schema(session):
+    Schema.create_object(session,{"NAME":cfg._config_schema})
+
+def create_config_stage(session):
+    InternalStage.create_object(session,{"NAME":cfg._config_internal_stage})
+
 
 def main(session):
     create_default_role(session)
     create_default_warehouse(session)
+    create_config_database(session)
+    create_config_schema(session)
+    create_config_stage(session)
 
     
     
--- src/vars/global_vars.py
+++ src/vars/global_vars.py
@@ -1,4 +1,6 @@
 
+
+
 class Account:
     def __init__(self):
         pass
@@ -38,15 +40,6 @@ class Database:
 class Role:
     def __init__(self):
         pass
-
-    _default_role = {
-        "RL_OWNER_EVERY_OBJ" : "Default role to be the owner of all the objects within account",
-        "RL_ALL_EVERY_OBJ" : "Default role to have all allowed privileges on all objects except ownership",
-        "RL_MONITOR_EVERY_OBJ" : "Default role to have mointor privileges on objects that allow monitor",
-        "RL_USAGE_EVERY_OBJECT" : "Default role to have usage privileges on objects that allow usage",
-        "RL_MODIFY_EVERY_OBJECT" : "Default role to have modify privileges on objects that allow modify",
-        "RL_OPERATE_EVERY_OBJ" : "Default role to have operate privileges on objects that allow operate privileges"
-    }
     _name_tag = "NAME"
     _comment_tag = "COMMENT"
 
@@ -237,18 +230,9 @@ class Warehouse:
     _statement_timeout_in_seconds_tag = "STATEMENT_TIMEOUT_IN_SECONDS"
     _allowed_values_warehouse_type = ["STANDARD","SNOWPARK-OPTIMIZED"]
     _allowed_values_warehouse_size = ["XSMALL","SMALL","MEDIUM","LARGE","XLARGE","XXLARGE","XXXLARGE","X4LARGE","X5LARGE","X6LARGE"]
-    _default_warehouse = {
-        "WH_XSMALL" : "XSMALL",
-        "WH_SMALL" : "SMALL",
-        "WH_MEDIUM" : "MEDIUM",
-        "WH_LARGE" : "LARGE",
-        "WH_XLARGE" : "XLARGE",
-        "WH_XXLARGE" : "XXLARGE"
-    }
 
 
 class Privileges:
-    _default_privilege_set = ["OWNERSHIP","ALL","MONITOR","MODIFY","USAGE","OPERATE"]
     _allowed_object_type = ["USER","ROLE","VIRTUAL_WAREHOUSE","DATABASE","SCHEMA","TABLE","FILE_FORMAT","PIPE","TASK"]
     _user_privileges = ["MONITOR","OWNERSHIP","ALL"]
     _role_privileges = ["OWNERSHIP"]
@@ -265,4 +249,30 @@ class Privileges:
     _file_format_privileges = ["USAGE","OWNERSHIP","ALL"]
     _pipe_privileges = ["APPLYBUDGET","MONITOR","OPERATE","OWNERSHIP","ALL"]
     _task_privileges = ["APPLYBUDGET","MONITOR","OPERATE","OWNERSHIP","ALL"]
+
+
+class Config:
+    def __init__(self):
+        pass
+    _config_database = "DB_CONFIG"
+    _config_schema = "CONFIG"
+    _config_internal_stage = "STAGE_INT_CONFIG"
+    _default_privilege_set = ["OWNERSHIP","ALL","MONITOR","MODIFY","USAGE","OPERATE"]
+    _default_warehouse = {
+        "WH_XSMALL" : "XSMALL",
+        "WH_SMALL" : "SMALL",
+        "WH_MEDIUM" : "MEDIUM",
+        "WH_LARGE" : "LARGE",
+        "WH_XLARGE" : "XLARGE",
+        "WH_XXLARGE" : "XXLARGE"
+    }
+    _default_role = {
+        "RL_OWNER_EVERY_OBJ" : "Default role to be the owner of all the objects within account",
+        "RL_ALL_EVERY_OBJ" : "Default role to have all allowed privileges on all objects except ownership",
+        "RL_MONITOR_EVERY_OBJ" : "Default role to have mointor privileges on objects that allow monitor",
+        "RL_USAGE_EVERY_OBJECT" : "Default role to have usage privileges on objects that allow usage",
+        "RL_MODIFY_EVERY_OBJECT" : "Default role to have modify privileges on objects that allow modify",
+        "RL_OPERATE_EVERY_OBJ" : "Default role to have operate privileges on objects that allow operate privileges"
+    }
+        
 
