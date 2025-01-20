7bec1cc0c026acf3ac4050599e5f54094893a7da
Priyank Malviya <priyankmalviya0@gmail.com>
2025-01-19 19:51:19 -0500

    fix import in initial.py

--- src/deploy/deploy.py
+++ src/deploy/deploy.py
@@ -1,6 +1,13 @@
+
+
 class DeployAttr:
+    file_name = FileName()
 
 
 class Deploy:
     def __init__(self):
         self.attr = DeployAttr()
+
+    def set_file_name(self)
+
+    def put_file_in_stage
--- src/setup/initial.py
+++ src/setup/initial.py
@@ -1,17 +1,13 @@
 import sys
 import os 
 
-sys.path.append(os.path.join(os.path.dirname(__file__),'../obj'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
 sys.path.append(os.path.join(os.path.dirname(__file__),'../vars'))
 
 from global_vars import Role as gv_rl
 from global_vars import Warehouse as gv_wh
 from global_vars import Config as cfg
-from role import Role
-from warehouse import Warehouse
-from database import Database
-from schema import Schema
-from internalstage import InternalStage
+from obj import role,warehouse,database,schema,internalstage
 
 
 class InitialSetup:
@@ -20,26 +16,26 @@ class InitialSetup:
         self.session = session
         
     def create_default_role(self):
-        for role,description in gv_rl._default_role.items():
-            Role.create_object(self.session,{"NAME":role,"COMMENT":description})
+        for roles,description in gv_rl._default_role.items():
+            role.Role.create_object(self.session,{"NAME":role,"COMMENT":description})
     
     def create_default_warehouse(self):
         for warehouse_name,warehouse_size in gv_wh.__default_warehouse.items():
-            Warehouse.create_object(self.session,{"NAME":warehouse_name,"TYPE":"STANDARD","SIZE": warehouse_size})
+            warehouse.Warehouse.create_object(self.session,{"NAME":warehouse_name,"TYPE":"STANDARD","SIZE": warehouse_size})
     
     def create_config_database(self):
-        Database.create_object(self.session,{"NAME":cfg._config_database})
+        database.Database.create_object(self.session,{"NAME":cfg._config_database})
     
     def create_config_schema(self):
-        Schema.create_object(self.session,{"NAME":cfg._config_schema})
+        schema.Schema.create_object(self.session,{"NAME":cfg._config_schema})
     
     def create_config_stage(self):
-        InternalStage.create_object(self.session,{"NAME":cfg._config_stage})
+        internalstage.InternalStage.create_object(self.session,{"NAME":cfg._config_stage})
     
     def create_deployment_stage(self):
-        InternalStage.create_object(self.session,{"NAME":cfg._deployment_stage})
+        internalstage.InternalStage.create_object(self.session,{"NAME":cfg._deployment_stage})
     
-    def perform_initial_setup():
+    def perform_initial_setup(self):
         self.create_default_role()
         self.create_default_warehouse()
         self.create_config_database()
