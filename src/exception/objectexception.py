18c199b9369450ae452b82463d37d2d744f18325
Priyank Malviya <priyankmalviya0@gmail.com>
2025-02-02 15:39:00 -0500

    add exception class for handling existing object names in users sf account

---
+++ src/exception/objectexception.py
@@ -0,0 +1,12 @@
+class ObjectException(Exception):
+    """Base class for attribute validation exceptions"""
+    def __init__(self, object_type, message):
+        self.object_type = object_type
+        self.message = message
+    
+    def __str__(self):
+        return f"ERROR : {self.object_type} object {self.message}"
+    
+class ObjectDoesNotExist(ObjectException):
+     def __init__(self, object_type,object_name):
+        super().__init__(object_type,f"with name {object_name} does not exist")
