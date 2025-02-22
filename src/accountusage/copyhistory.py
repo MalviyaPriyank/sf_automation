8fc485155f59c0a6209449549672c360cc66e2dc
Priyank Malviya <priyankmalviya0@gmail.com>
2025-02-22 15:52:15 -0500

    adhoc fix for get_load_history_for_a_table

--- src/accountusage/copyhistory.py
+++ src/accountusage/copyhistory.py
@@ -22,11 +22,16 @@ class CopyHistory:
         return df
         
     def get_load_history_for_a_table(self,table_db,table_schema,table_name):
+        """
         df=self.session.table(self.attr._view)\
                     .filter(col((self.attr._table_catalog_name)==table_db)\
                             & (col(self.attr._table_schema_name)==table_schema)\
                             &(col(self.attr._table_name))==table_name)\
                     .select(col(self.attr._status),col(self.attr._file_name),col(self.attr._row_count))\
                     .sort(col(self.attr._last_load_time).desc()).limit(10)
+        """
+        self.session.sql(f"use database {table_db}").collect()
+        self.session.sql(f"use schema {table_schema}").collect()
+        df=self.session.sql(f"select * from table(information_schema.copy_history(TABLE_NAME=>{table_name}, START_TIME=> DATEADD(hours, -24, CURRENT_TIMESTAMP())))").collect()
         return df
     
