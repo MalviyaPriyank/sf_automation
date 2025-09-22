f4252be457b2adf9784b911ff34f324a67eb1ae9
Sharmin Pathan <sharmin@Sharmins-MacBook-Air.local>
2025-09-21 20:43:56 -0700

    analysis fix

--- src/model/.ipynb_checkpoints/tools-checkpoint.py
+++ src/model/.ipynb_checkpoints/tools-checkpoint.py
@@ -693,14 +693,9 @@ class LLMTools:
     def perform_data_analysis(self, query, table_name='Customer_Loyalty_History'):
             table_name = (table_name.replace(' ', '_')).lower()
             self.logger.info(f'table_name: {table_name}')
-            table_data = pd.read_csv(f'analysis/{table_name.lower()}.csv')
+            table_data = pd.read_csv(f'analysis/sales_pipeline.csv')
     
-            prompt = f"""The customer loyalty history table has columns: {list(table_data.columns)}.
-            Here is head of the table: {table_data.head().to_string()}. Please read the entire table with table_data = pd.read_csv('analysis/{table_name.upper()}.csv').
-            Write a Python script for: {query}. Only return code inside <python></python> tags.
-            if creating any visualizations or output csv, save them inside 'analysis' folder.
-            be sure to check for and handle missing data.
-            """
+            prompt = f"""Write a Python script for: {query}. table data is in file analysis/sales_pipeline.csv, do not attempt to read from any other file. The columns are opportunity_id, sales_agent, product, account, deal_stage, engage_date, close_date, close_value. Only return code inside <python></python> tags. Handle missing values. if creating any visualizations or output csv, save them inside 'analysis' folder. be sure to check for and handle missing data."""
 
             client = boto3.client(llm_config.BEDROCK_RUNTIME_SERVICE,
                                    aws_access_key_id=llm_config.ACCESS_KEY,
