6e1236479cad61fc4ed8e56d8841bd70cd5af969
Sharmin Pathan <sharmin@Sharmins-MacBook-Air.local>
2025-12-06 12:58:15 -0800

    no tag for db

--- src/vars/base/.ipynb_checkpoints/basetag-checkpoint.py
+++ src/vars/base/.ipynb_checkpoints/basetag-checkpoint.py
@@ -18,7 +18,7 @@ class BaseTag:
             "NAME":"this is a dictionary data type with keys {'NAME', 'RENAME_TO'} - pass value as a dictionary object. This is to be taken as input from user. DO NOT ASSUME values for NAME and RENAME_TO, these are to be taken from user. the format is a dictionary with key 'NAME' for the name, and 'RENAME_TO' key when the ask is to alter (IS_CREATE=TRUE).",
             "COMMENT":"This will be user defined comment for the object. If user does not define one add a proper comment as per your understanding and inform the user.",
             "IS_CREATE":"Set this to 'TRUE' if create object. set to 'FALSE' if alter object.",
-            "OBJECT_TAG":"this is a required attribute. this is a dictionary with one or more pairs of {'NAME':'VALUE'}. use get_tag_objects tool to get the list of tag objects, if none fit the need you can create a new tag object and pass it to this parameter."
+            "OBJECT_TAG":"this is not required for database object but required attribute otherwise. this is a dictionary with one or more pairs of {'NAME':'VALUE'}. use get_tag_objects tool to get the list of tag objects, if none fit the need you can create a new tag object and pass it to this parameter."
         }
 
 @dataclass(frozen=True)
